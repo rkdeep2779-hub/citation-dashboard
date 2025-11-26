1d8c2d16476e8e357a04163c15d58ce7709e6864
rkdeep2779-hub <rkdeep2779@gmail.com>
2025-11-27 00:22:27 +0530

    fix: correct path for supabaseClient in directories api

--- pages/api/directories/[id].js
+++ pages/api/directories/[id].js
@@ -1,4 +1,4 @@
-import { supabase } from "../../../../lib/supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 
 export default async function handler(req, res) {
   const { id } = req.query;
